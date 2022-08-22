a220dbbddd1131dbfa7bfa236523390992e84789
hasubasora <330882327@qq.com>
2022-08-22 17:28:38 +0900

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { OrderComponent } from './order/order.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'order', component: OrderComponent },
+  { path: 'myOrder', component: MyOrderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,17 @@
 import { NgModule } from '@angular/core';//核心
 import { BrowserModule } from '@angular/platform-browser';//浏览器核心
 import { FormsModule } from '@angular/forms'; //表单
-import { RouterModule, Routes } from "@angular/router";
+
+import { AppRoutingModule } from './app-routing.module';
 // //导入每个模板ts
 import { AppComponent } from './app.component';
 import { OrderComponent } from './order/order.component';
 import { MyOrderComponent } from './my-order/my-order.component';
-// import { OrderComponent } from './order/order.component';
-// import { FirstComponent } from './first/first.component';
-// import { SecondComponent } from './second/second.component';
-// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+
+//****************移动到单独路由 ****************/
+// import { RouterModule, Routes } from "@angular/router";
 // const appRoutes: Routes = [
 //   { path: 'order', component: OrderComponent },
 //   { path: 'first', component: FirstComponent },
@@ -19,27 +20,26 @@ import { MyOrderComponent } from './my-order/my-order.component';
 //   // { path: 'second', redirectTo:'first', pathMatch:'full' },//重定向
 //   // { path: '**', component: PageNotFoundComponent },//错误匹配
 // ]
-
+//****************移动到单独路由 ****************/
 
 // //装饰器
 @NgModule({
   declarations: [//把模板组件加载出来
-    AppComponent, OrderComponent, MyOrderComponent,
-    // OrderComponent,
-    // FirstComponent,
-    // SecondComponent,
-    // PageNotFoundComponent
+    AppComponent, OrderComponent, MyOrderComponent, PageNotFoundComponent,
   ],
   imports: [//使用的模型依赖
     BrowserModule
     , FormsModule//注册表单模型
-    ,RouterModule.forRoot([
-      // {path: 'crisis-list', component: OrderComponent},
-      // {path: 'heroes-list', component: FirstComponent},
-    ])
-//     , RouterModule.forRoot(appRoutes)//appRouter，根节点有效
+    , AppRoutingModule//注册路由
+    //****************移动到单独路由 ****************/
+    // ,RouterModule.forRoot([
+    // {path: 'crisis-list', component: OrderComponent},
+    // {path: 'heroes-list', component: FirstComponent},
+    // ])
+    //     , RouterModule.forRoot(appRoutes)//appRouter，根节点有效
+    //****************移动到单独路由 ****************/
   ],
-//   //提供者
+  //   //提供者
   providers: [],//配置项目需要服务
   bootstrap: [AppComponent]//主控件
 })
